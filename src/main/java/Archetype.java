61840bd495636665d49d907427c311e5cc8c4d91
pavelkor91 <pavelkor91@gmail.com>
2017-04-09 20:46:54 +0300

    Homework for archetype

---
+++ src/main/java/Archetype.java
@@ -0,0 +1,43 @@
+
+//package org.docx4j.samples;
+
+
+import java.util.HashMap;
+
+import org.docx4j.XmlUtils;
+import org.docx4j.openpackaging.io.SaveToZipFile;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Document;
+
+public class Archetype {
+
+    public static void main(String[] args) throws Exception {
+
+        String inputfilepath = System.getProperty("user.dir") + "/src/main/resources/inputFile.docx";
+        boolean save = true;
+        String outputfilepath = System.getProperty("user.dir")
+                + "/src/main/resources/output.docx";
+
+        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage
+                .load(new java.io.File(inputfilepath));
+        MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+        String xml = XmlUtils.marshaltoString(documentPart.getJaxbElement(), true);
+        HashMap<String, String> mappings = new HashMap<String, String>();
+        mappings.put("name", "world");
+
+        Object obj = XmlUtils.unmarshallFromTemplate(xml, mappings);
+
+        documentPart.setJaxbElement((Document) obj);
+
+        // Save it
+        if (save) {
+            SaveToZipFile saver = new SaveToZipFile(wordMLPackage);
+            saver.save(outputfilepath);
+        } else {
+            System.out.println(XmlUtils.marshaltoString(documentPart.getJaxbElement(), true,
+                    true));
+        }
+    }
+
+}
